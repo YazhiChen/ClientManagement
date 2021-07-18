6490335d44e6a46b833d8d520484d58f7eb7ff20
Yazhi Chen <yche0338@student.monash.edu>
2021-07-19 02:37:50 +1000

    Add project files.

---
+++ Models/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientManagement.Models
+{
+    public class Client
+    {
+        [Key]
+        public int ClientID { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        [Required]
+        public string Name { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        public string Address { get; set; }
+        [Column(TypeName = "nvarchar(30)")]
+        [Required]
+        public string City { get; set; }
+        [Column(TypeName = "nvarchar(20)")]
+        [Required]
+        public string State { get; set; }
+        [Column(TypeName = "nvarchar(10)")]
+        [Required]
+        public string PostCode { get; set; }
+        [Column(TypeName = "nvarchar(30)")]
+        [Required]
+        public string Country { get; set; }
+
+    }
+}
---
+++ Models/ClientDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientManagement.Models
+{
+    public class ClientDbContext:DbContext
+    {
+        public ClientDbContext(DbContextOptions<ClientDbContext>options):base(options)
+        {
+        }
+        public DbSet<Client> Clients { get; set; }
+    
+
+    }
+}
